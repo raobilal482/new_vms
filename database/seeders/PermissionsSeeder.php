beed229885afaed0fc8597a5a24626a5165c4231
Rao Bilal <raobilal482@gmail.com>
2025-02-27 00:25:53 +0500

    permission error

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
+// use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class PermissionsSeeder extends Seeder
