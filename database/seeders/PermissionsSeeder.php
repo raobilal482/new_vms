b3cd2908b1b2481b2d0861fb85a1337034a7681e
Rao Bilal <raobilal482@gmail.com>
2025-02-27 00:29:35 +0500

    per

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -1,52 +1,51 @@
 <?php
-
 namespace Database\Seeders;
 
 use App\Models\Permission;
 use Illuminate\Database\Seeder;
-// use Spatie\Permission\Models\Permission;
-use Spatie\Permission\Models\Role;
 
 class PermissionsSeeder extends Seeder
 {
     public function run()
     {
-            $permissions = [
-                'create event',
-                'edit event',
-                'delete event',
-                'view event',
-                'approve event',
+        $permissions = [
+            'create event',
+            'edit event',
+            'delete event',
+            'view event',
+            'approve event',
 
-                'create task',
-                'edit task',
-                'delete task',
-                'view task',
+            'create task',
+            'edit task',
+            'delete task',
+            'view task',
 
-                'create time tracking',
-                'edit time tracking',
-                'delete time tracking',
-                'view time tracking',
+            'create time tracking',
+            'edit time tracking',
+            'delete time tracking',
+            'view time tracking',
 
-                'create role',
-                'edit role',
-                'delete role',
-                'view role',
+            'create role',
+            'edit role',
+            'delete role',
+            'view role',
 
-                'create permission',
-                'edit permission',
-                'delete permission',
-                'view permission',
+            'create permission',
+            'edit permission',
+            'delete permission',
+            'view permission',
 
-                'create user',
-                'edit user',
-                'delete user',
-                'view user',
-            ];
+            'create user',
+            'edit user',
+            'delete user',
+            'view user',
+        ];
 
-            foreach ($permissions as $permission) {
-                Permission::firstOrCreate(['name' => $permission,'guard_name' => 'web']);
-            }
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission,
+                'guard_name' => 'web'  
+            ]);
         }
     }
-
+}
